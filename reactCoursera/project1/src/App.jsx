1ea1ce3d9022bedc681f6e119a46f0f691c488c5
sandeeprout8797 <sandeeprout8797@gmail.com>
2026-03-23 11:36:43 +0530

    uodate

---
+++ reactCoursera/project1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  let name = "Sandeep";
+
+  return (
+    <div className="App">
+      <h1>Hello {name}</h1>
+      <p>Welcome to React Project</p>
+    </div>
+  );
+}
+
+export default App;
